52ddf0671f5dae6b6fd9cfef88f7e98901fdbe69
v0o0v <v0o0v@daum.net>
2020-04-01 19:49:48 +0900

    - add service classes - add class that make test data

---
+++ src/main/java/com/oscar/backend/service/ContactService.java
@@ -0,0 +1,45 @@
+package com.oscar.backend.service;
+
+import com.oscar.backend.entity.Contact;
+import com.oscar.backend.repository.CompanyRepository;
+import com.oscar.backend.repository.ContactRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
+public class ContactService {
+    private static final Logger LOGGER = Logger.getLogger(ContactService.class.getName());
+    private ContactRepository contactRepository;
+    private CompanyRepository companyRepository;
+
+    public ContactService(ContactRepository contactRepository,
+                          CompanyRepository companyRepository) {
+        this.contactRepository = contactRepository;
+        this.companyRepository = companyRepository;
+    }
+
+    public List<Contact> findAll() {
+        return contactRepository.findAll();
+    }
+
+    public long count() {
+        return contactRepository.count();
+    }
+
+    public void delete(Contact contact) {
+        contactRepository.delete(contact);
+    }
+
+    public void save(Contact contact) {
+        if (contact == null) {
+            LOGGER.log(Level.SEVERE,
+                    "Contact is null. Are you sure you have connected your form to the application?");
+            return;
+        }
+        contactRepository.save(contact);
+    }
+
+}
