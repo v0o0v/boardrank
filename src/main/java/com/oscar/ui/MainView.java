6e6e253c569876c012993aa20ece60c0c0574522
v0o0v <v0o0v@daum.net>
2020-04-01 17:24:16 +0900

    - add entities

---
+++ src/main/java/com/oscar/ui/MainView.java
@@ -0,0 +1,15 @@
+package com.oscar.ui;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+
+    }
+
+}
