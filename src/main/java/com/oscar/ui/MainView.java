354400f9ee67c5b1f2af4048b7ab4b118de785bd
v0o0v <v0o0v@daum.net>
2020-04-02 17:03:59 +0900

    필터 추가

--- src/main/java/com/oscar/ui/MainView.java
+++ src/main/java/com/oscar/ui/MainView.java
@@ -5,31 +5,41 @@ import com.oscar.backend.entity.Contact;
 import com.oscar.backend.service.ContactService;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 
 @Route("")
 public class MainView extends VerticalLayout {
 
     private ContactService contactService;
+
     private Grid<Contact> grid = new Grid<>(Contact.class);
+    private TextField filterText = new TextField();
 
     public MainView(ContactService contactService) {
         this.contactService = contactService;
 
         addClassName("list-view");
         setSizeFull();
+        configureFilter();
         configureGrid();
 
-        add(grid);
+        add(filterText, grid);
         updateList();
+    }
+
+    private void configureFilter() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
     }
 
     private void configureGrid() {
         grid.addClassName("contact-grid");
         grid.setSizeFull();
-        grid.setColumns("firstName", "lastName", "email", "status","company");
+        grid.setColumns("firstName", "lastName", "email", "status", "company");
 
         grid.removeColumnByKey("company");
 
@@ -42,6 +52,6 @@ public class MainView extends VerticalLayout {
     }
 
     private void updateList() {
-        grid.setItems(this.contactService.findAll());
+        grid.setItems(this.contactService.findAll(this.filterText.getValue()));
     }
 }
