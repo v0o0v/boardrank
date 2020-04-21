3546c3bf27b7bbf9a773b9e33ea70706174659ac
v0o0v <v0o0v@daum.net>
2020-04-21 20:41:18 +0900

    - 현재 인증 유저가 참여한 게임 리스트 가져오기 추가

---
+++ src/main/java/net/boardrank/boardgame/service/GameService.java
@@ -0,0 +1,31 @@
+package net.boardrank.boardgame.service;
+
+import net.boardrank.account.domain.Account;
+import net.boardrank.account.domain.repository.AccountRepository;
+import net.boardrank.boardgame.domain.Game;
+import net.boardrank.boardgame.domain.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    GameRepository gameRepository;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    public List<Game> getGamesOfCurrentSessionAuth() {
+        UserDetails details = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Account account = accountRepository.findByEmail(details.getUsername()).orElseThrow(RuntimeException::new);
+
+        List<Game> gamesByPaticiantAccountsContaining = this.gameRepository.findGamesByPaticiantAccountsContaining(account);
+        return gamesByPaticiantAccountsContaining;
+    }
+
+}
