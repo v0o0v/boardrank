7156257bf8915a66cddc0fdb0df37d417d6db227
v0o0v <v0o0v@daum.net>
2020-04-28 13:49:42 +0900

    - 메인화면 레이아웃 정리 및 아이콘 추가

--- src/main/java/net/boardrank/boardgame/ui/MainLayout.java
+++ src/main/java/net/boardrank/boardgame/ui/MainLayout.java
@@ -11,6 +11,8 @@ import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.PageTitle;
 
 @CssImport("./styles/shared-styles.css")
@@ -26,7 +28,7 @@ public class MainLayout extends AppLayout {
         H1 logo = new H1("Board Rank");
         logo.addClassName("logo");
 
-        Anchor logout = new Anchor("/logout", "Log out");
+        Anchor logout = new Anchor("/logout", new Icon(VaadinIcon.EXIT_O));
 
         Button createMatch = new Button("Create New Match");
         MatchCreateDialog matchCreateDialog = new MatchCreateDialog();
@@ -42,22 +44,23 @@ public class MainLayout extends AppLayout {
     }
 
     private void createDrawer() {
-//        RouterLink link_history = new RouterLink("Match History", GameListView.class);
-//        link_history.setHighlightCondition(HighlightConditions.sameLocation());
+        Tabs tabs = new Tabs();
 
         Button btn_myRank = new Button("My Rank" , new Icon(VaadinIcon.CHART_3D));
         btn_myRank.addClickListener( e -> UI.getCurrent().navigate(GameListView.class));
         btn_myRank.setWidthFull();
-        addToDrawer(btn_myRank);
+        tabs.add(new Tab(btn_myRank));
 
         Button btn_matchHistory = new Button("Match History" , new Icon(VaadinIcon.LINE_CHART));
         btn_matchHistory.addClickListener( e -> UI.getCurrent().navigate(GameListView.class));
         btn_matchHistory.setWidthFull();
-        addToDrawer(btn_matchHistory);
+        tabs.add(new Tab(btn_matchHistory));
 
         Button btn_friend = new Button("My Friends" , new Icon(VaadinIcon.GROUP));
         btn_friend.addClickListener( e -> UI.getCurrent().navigate(FriendListView.class));
         btn_friend.setWidthFull();
-        addToDrawer(btn_friend);
+        tabs.add(new Tab(btn_friend));
+
+        addToDrawer(tabs);
     }
 }
