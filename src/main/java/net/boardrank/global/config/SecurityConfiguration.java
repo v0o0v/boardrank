8b5c89aa86b06d4772c931f2700dc0f7d62e5926
v0o0v <v0o0v@daum.net>
2020-06-11 14:49:50 +0900

    - sprigboot 2.2.0 -> 2.2.6

--- src/main/java/net/boardrank/global/config/SecurityConfiguration.java
+++ src/main/java/net/boardrank/global/config/SecurityConfiguration.java
@@ -1,28 +1,18 @@
 package net.boardrank.global.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-//    private static final String LOGIN_PROCESSING_URL = "/login";
-//    private static final String LOGIN_FAILURE_URL = "/login?error";
-//    private static final String LOGIN_URL = "/login";
-//    private static final String LOGOUT_SUCCESS_URL = "/login";
-
     private static final String LOGIN_URL = "/login";
-    private static final String LOGOUT_URL = "/logout";
-    private static final String LOGOUT_SUCCESS_URL = "/";
+    private static final String LOGOUT_SUCCESS_URL = "/login";
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,7 +27,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .oauth2Login()
                         .loginPage(LOGIN_URL).permitAll()
                 .and()
-                    .logout().logoutUrl(LOGOUT_URL).logoutSuccessUrl(LOGOUT_SUCCESS_URL);
+                    .logout().logoutSuccessUrl(LOGOUT_SUCCESS_URL);
     }
 
     @Override
