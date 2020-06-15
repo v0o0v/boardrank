76b1231dca3374bc9ab2ffeecb8e7a44659b90c1
v0o0v <v0o0v@daum.net>
2020-06-15 21:24:41 +0900

    - 매치 결과 accept notice에 user button 적용

--- src/main/java/net/boardrank/boardgame/ui/todo/NoticeAcceptMatchResultView.java
+++ src/main/java/net/boardrank/boardgame/ui/todo/NoticeAcceptMatchResultView.java
@@ -12,13 +12,14 @@ import com.vaadin.flow.data.renderer.ComponentRenderer;
 import net.boardrank.boardgame.domain.*;
 import net.boardrank.boardgame.service.GameMatchService;
 import net.boardrank.boardgame.ui.common.ResponsiveVerticalLayout;
+import net.boardrank.boardgame.ui.common.UserButton;
 
 import java.time.format.DateTimeFormatter;
 
 public class NoticeAcceptMatchResultView extends ResponsiveVerticalLayout {
 
     GameMatchService gameMatchService;
-    
+
     Notice notice;
 
     Account me;
@@ -37,10 +38,9 @@ public class NoticeAcceptMatchResultView extends ResponsiveVerticalLayout {
         H3 lbl_title = new H3("Match 결과 승인 요청");
         add(lbl_title);
         add(new Label(notice.getCreatedTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))));
-        
+
         add(new HorizontalLayout(
-                new Button(notice.getFrom().getName(), event -> {
-                })
+                new UserButton(gameMatchService, notice.getFrom())
                 , new Label("님께서 Match 결과 승인을 요청하셨습니다.")
         ));
         GameMatch gameMatch = notice.getGameMatch();
@@ -48,15 +48,14 @@ public class NoticeAcceptMatchResultView extends ResponsiveVerticalLayout {
         VerticalLayout match = new VerticalLayout();
         add(match);
         match.setDefaultHorizontalComponentAlignment(Alignment.START);
-        match.add(new H6("■ 보드게임 : "+gameMatch.getFullBoardgameString()));
-        match.add(new H6("■ 게임장소 : "+gameMatch.getPlace()));
-        match.add(new H6("■ 종료시간 : "+gameMatch.getFinishedTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))));
+        match.add(new H6("■ 보드게임 : " + gameMatch.getFullBoardgameString()));
+        match.add(new H6("■ 종료시간 : " + gameMatch.getFinishedTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))));
 
         Grid<RankEntry> gridParty = new Grid<>();
         gridParty.removeAllColumns();
         gridParty.setWidthFull();
         gridParty.addColumn(new ComponentRenderer<>(rankEntry -> {
-            VerticalLayout layout = new VerticalLayout(new Button(rankEntry.getAccount().getName()));
+            VerticalLayout layout = new VerticalLayout(new UserButton(gameMatchService,rankEntry.getAccount()));
             layout.setDefaultHorizontalComponentAlignment(Alignment.START);
             return layout;
         })).setHeader("참가자");
@@ -69,7 +68,7 @@ public class NoticeAcceptMatchResultView extends ResponsiveVerticalLayout {
         });
         gridParty.setItems(gameMatch.getRankentries());
         match.add(gridParty);
-        
+
         add(new HorizontalLayout(
                 new Button("거절하기", event -> {
                     gameMatchService.handleMatchAccept(notice, NoticeResponse.Deny, me);
